a5d4b8d4cee20c455800fb6f040e89bd1b1c4c4c
heipartho05 <heipartho72@gmail.com>
2022-01-12 22:36:37 +0600

    update Nthfibo and calculatorProgram

--- src/com/partho/NthFiboNum.java
+++ src/com/partho/NthFiboNum.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class NthFiboNum {
     public static void main(String[] args) {
 
+        // fibo series ==> 0, 1, 1, 2, 3, 5, 8, 13.......
         // 7th fibo number is 13 ==> then use count <= n
         // 7th fibo num is 8 ==> then use count < n
 
