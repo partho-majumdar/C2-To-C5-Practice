db03603fd1d137956eba34541169d890e5fadace
heipartho05 <heipartho72@gmail.com>
2022-01-12 22:31:00 +0600

    change something in armstrongRange

--- src/com/partho/ArmstrongRange.java
+++ src/com/partho/ArmstrongRange.java
@@ -1,2 +1,24 @@
-package com.partho;public class ArmstrongRange {
+package com.partho;
+
+public class ArmstrongRange {
+    public static void main(String[] args) {
+
+        for (int i = 100; i < 1000; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int temp = n % 10;
+            sum = sum + (temp * temp * temp);
+            n = n / 10;
+        }
+        return sum == original;
+    }
 }
